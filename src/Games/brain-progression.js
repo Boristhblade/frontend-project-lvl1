b93bf17a09f1500822a4e4fcf895571132cda7a0
Boris <watasinonamaewa@gmail.com>
2021-11-29 17:50:06 +1000

    fix brain-progression

--- src/Games/brain-progression.js
+++ src/Games/brain-progression.js
@@ -1,19 +1,19 @@
 import { getRandomNumber } from '../cli.js';
 
 const generateProgression = () => {
-  const array = [];
-  const arrayStart = getRandomNumber(0, 50);
-  const arrayStep = getRandomNumber(1, 10);
-  array.push(arrayStart);
-  while (array.length < getRandomNumber(0, 5) + 5) {
-    array.push(array[array.length - 1] + arrayStep);
+  const progression = [];
+  const startNumber = getRandomNumber(0, 50);
+  const progressionStep = getRandomNumber(1, 10);
+  progression.push(startNumber);
+  while (progression.length < getRandomNumber(0, 5) + 5) {
+    progression.push(progression[progression.length - 1] + progressionStep);
   }
-  return array;
+  return progression;
 };
 
 const generateProgressionQuestion = () => {
   const progression = generateProgression();
-  const correctAnswer = progression[getRandomNumber(progression.length - 1)];
+  const correctAnswer = progression[getRandomNumber(0, progression.length - 1)];
   progression[progression.indexOf(correctAnswer)] = '..';
   const question = progression.join(' ');
   return [question, String(correctAnswer)];
