821b7e7e4cfb83c2558f47f760ec91b7845fcae2
Boris <watasinonamaewa@gmail.com>
2021-11-29 17:25:52 +1000

    fix brain-progression

--- src/Games/brain-progression.js
+++ src/Games/brain-progression.js
@@ -5,7 +5,7 @@ const generateProgression = () => {
   const arrayStart = getRandomNumber(0, 50);
   const arrayStep = getRandomNumber(1, 10);
   array.push(arrayStart);
-  while (array.length < getRandomNumber(5) + 5) {
+  while (array.length < getRandomNumber(0, 5) + 5) {
     array.push(array[array.length - 1] + arrayStep);
   }
   return array;
